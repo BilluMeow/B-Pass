1b3047e48ea9768b6ce264deb5cc8b0014a0f168
BilluMeow <syed.abeer.hussain.23@gmail.com>
2022-09-09 16:52:17 +0500

    Implemented password strength checker
    Also removed an unused import

--- fileprocessor.py
+++ fileprocessor.py
@@ -1,13 +1,13 @@
 from core import AESCipher
 import pickle
-
-import json
+import re
 
 class Encryptor:
     def __init__(self) -> None:
         self.cipher = AESCipher()
 
     def encrypt(self, filename : str, password : str) -> bool:
+        assert self.password_strength_checker(password) == True
         with open(filename, 'rb') as original_file:
 
             data = original_file.read()
@@ -18,6 +18,7 @@ class Encryptor:
 
 
     def decrypt(self, filename : str, password : str) -> bool:
+        assert self.password_strength_checker(password) == True
         with open(filename, 'rb') as original_file:
 
             encrypted_data = pickle.load(original_file)
@@ -27,9 +28,23 @@ class Encryptor:
                 exporting_file.write(decrypted_data)
 
     def password_strength_checker(self, password : str) -> bool:
-
         if len(password) < 21:
             raise Exception('Password too small')
+        
+        if not re.search("[a-z]", password):
+            raise Exception('Password should contain atleast 1 small letter')
+
+        if not re.search("[A-Z]", password):
+            raise Exception('Password should contain atleast 1 capital letter')
+
+        if not re.search("[0-9]", password):
+            raise Exception('Password should contain atleast 1 number')
+
+        if not re.search("[!@#$%^&*?~]", password):
+            raise Exception('Password should contain atleast 1 special character')
+
+        if re.search("[^!@#$%^&*?~0-9A-Za-z]", password):
+            raise Exception('Password should not contain any of these : ()-_=+[]{/}\\;:\'\".,<>|`')
 
         return True
 
