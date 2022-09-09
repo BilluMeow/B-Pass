675fd30b8589678881f9587d2eeb2dcb6dab6b54
BilluMeow <syed.abeer.hussain.23@gmail.com>
2022-09-09 16:53:42 +0500

    optimized the imports

--- fileprocessor.py
+++ fileprocessor.py
@@ -1,6 +1,6 @@
 from core import AESCipher
-import pickle
-import re
+from pickle import load, dumps
+from re import search
 
 class Encryptor:
     def __init__(self) -> None:
@@ -14,14 +14,14 @@ class Encryptor:
             encrypted_data = self.cipher.encrypt(data, password)
             
             with open(filename[:filename.rfind('.')]+'_encrypted.dat', 'wb') as exporting_file:
-                exporting_file.write(pickle.dumps(encrypted_data))
+                exporting_file.write(dumps(encrypted_data))
 
 
     def decrypt(self, filename : str, password : str) -> bool:
         assert self.password_strength_checker(password) == True
         with open(filename, 'rb') as original_file:
 
-            encrypted_data = pickle.load(original_file)
+            encrypted_data = load(original_file)
             decrypted_data = self.cipher.decrypt(encrypted_data, password)
             
             with open(filename[:filename.rfind('_')]+'_decrypted.csv', 'wb') as exporting_file:
@@ -31,19 +31,19 @@ class Encryptor:
         if len(password) < 21:
             raise Exception('Password too small')
         
-        if not re.search("[a-z]", password):
+        if not search("[a-z]", password):
             raise Exception('Password should contain atleast 1 small letter')
 
-        if not re.search("[A-Z]", password):
+        if not search("[A-Z]", password):
             raise Exception('Password should contain atleast 1 capital letter')
 
-        if not re.search("[0-9]", password):
+        if not search("[0-9]", password):
             raise Exception('Password should contain atleast 1 number')
 
-        if not re.search("[!@#$%^&*?~]", password):
+        if not search("[!@#$%^&*?~]", password):
             raise Exception('Password should contain atleast 1 special character')
 
-        if re.search("[^!@#$%^&*?~0-9A-Za-z]", password):
+        if search("[^!@#$%^&*?~0-9A-Za-z]", password):
             raise Exception('Password should not contain any of these : ()-_=+[]{/}\\;:\'\".,<>|`')
 
         return True
