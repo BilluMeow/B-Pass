0afa878e5d8c845b25c96c783072cf8baeb40ece
BilluMeow <syed.abeer.hussain.23@gmail.com>
2022-11-21 22:46:30 +0500

    Simple bug fix

--- encryption/core.py
+++ encryption/core.py
@@ -1,8 +1,8 @@
 # AES 256 encryption/decryption using pycryptodome library
 
 from base64 import b64encode, b64decode
-from Cryptodome.Cipher import AES
-from Cryptodome.Random import get_random_bytes
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 from hashlib import scrypt
 
 class AESCipher:
